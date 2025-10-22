d8a9a3acef4ddfd5fec8f7a7e6a7a4872823187c
cloudygreybeard <192177508+cloudygreybeard@users.noreply.github.com>
2025-10-22 14:16:12 +1100

    Update version to 0.1.4

--- Sources/Pastedown/Version.swift
+++ Sources/Pastedown/Version.swift
@@ -2,6 +2,6 @@
 // This file is automatically updated during the build process
 
 struct Version {
-    static let version = "0.1.3"
+    static let version = "0.1.4"
     static let buildDate = "2025-10-22"
 }
